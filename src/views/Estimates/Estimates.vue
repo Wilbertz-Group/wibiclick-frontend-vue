79eba1d69cbfc87c77e09a4306d6ddf84c4cf27c
Wilbert Muza <mrwilbertmuza@gmail.com>
2022-11-22 21:23:52 +0200

    Update Estimates.vue

--- src/views/Estimates/Estimates.vue
+++ src/views/Estimates/Estimates.vue
@@ -375,7 +375,7 @@
                                   <p :class="colors[column.title]" class="text-sm text-white rounded-full shadow-md px-3 py-1.5 my-1 w-fit">{{item.reason}}</p>
                                 </div>
 
-                                <p class="text-lg text-black -mt-3 mb-3">{{item.employee?.firstName + ' ' + item.employee?.lastName}}</p>
+                                <!-- <p class="text-lg text-black -mt-3 mb-3">{{item.employee?.firstName + ' ' + item.employee?.lastName}}</p> -->
 
                                 <div class="mt-3 mb-4 p-2 shadow rounded-2xl bg-slate-100">
                                   <div class="flex justify-between">
